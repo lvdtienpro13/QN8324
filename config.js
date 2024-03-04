2b924583baf784fddd2889ef391c469af6502a67
Nau <lvdtienpro13@gmail.com>
2024-03-04 22:38:57 +0700

    xyz

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
-const nameGirl = 'TT';
+const nameGirl = 'Thanh Tâm';
 const giftUrl = 'https://www.facebook.com/levietdaitien';
-const eventName = 'Happy women dayy';
+const eventName = 'Happy women dayy 🎉';
 const titleCard = 'Tặng em <3';
-const contentCard = ' 🎉💖';
+const contentCard = 'Chúc mừng ngày Quốc tế Phụ nữ 8/3! Chúc Tâm luôn được bao bọc bởi hạnh phúc, sức khỏe và thành công. Mình biết bạn vốn là kiểu người có thể tỏa sáng một mình, nhưng nếu bạn cần mặt trời 🌞 dù thế nào đi nữa mình cũng sẽ tới và dẫn lối cho bạn.  🎉💖';
 
-const giftImage = 'abc.jpg';
+const giftImage = 'QN11.png';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
