575fcedc96bbfac35ccaca50119cf58d9c947bed
Nau <lvdtienpro13@gmail.com>
2024-03-04 22:06:44 +0700

    abc

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'TT';
+const giftUrl = 'https://www.facebook.com/levietdaitien';
+const eventName = 'Happy women dayy';
+const titleCard = 'Tặng em <3';
+const contentCard = ' 🎉💖';
+
+const giftImage = 'abc.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
